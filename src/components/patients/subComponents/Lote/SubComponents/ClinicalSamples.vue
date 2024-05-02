ecbe42673e2e56d125fae17838bdb7a0f6585dce
csalgado <coordproyectosticsocbp@gmail.com>
2024-05-02 11:19:05 -0500

    clinical samples 02-05-2024

--- src/components/patients/subComponents/Lote/SubComponents/ClinicalSamples.vue
+++ src/components/patients/subComponents/Lote/SubComponents/ClinicalSamples.vue
@@ -1,11 +1,31 @@
 <script setup>
 
-import {computed, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
+import BatchService from "@/services/batchs/Batch.service.js";
+import {useStore} from "vuex";
+import {toast} from "vue3-toastify";
+import {getError} from "@/utils/helpers/getError.js";
+import {useLoading} from "vue-loading-overlay";
 
-const regex = /^MU-[0-9]{1,9}$/
+//const regex = /^MU-[0-9]{1,9}$/ -- Original
+const fullPage = ref(true)
+const regex = /^MU[0-9]{1,9}-\w{1,11}-\d-\d$/
 const clinicalSamples = ref([])
 const sampleCode = ref("")
 const filter = ref("")
+const errors = ref(null)
+const store = useStore()
+const authUser = computed(() => store.getters["auth/authUser"])
+
+const $loading = useLoading({
+  loader: 'dots',
+  isFullPage: fullPage,
+  width: 64,
+  height: 64,
+  backgroundColor: '#ffffff',
+  opacity: 0.5,
+  zIndex: 999,
+})
 
 const filteredItems = computed(() => {
   return filter.value
@@ -15,6 +35,23 @@ const filteredItems = computed(() => {
           }) : clinicalSamples.value
 })
 
+const getTemporalBatches = async () => {
+  const loader = $loading.show()
+  try {
+    const response = await BatchService.getTemporalBatches(authUser.value.id, 1)
+    if (response) {
+      clinicalSamples.value = response.data.data.temp_Muestras
+      loader.hide()
+    } else {
+      toast.error(response.data.message)
+      loader.hide()
+    }
+  } catch (error) {
+    loader.hide()
+    errors.value = getError(error)
+  }
+
+}
 const addItemToClinicalSamplesArray = () => {
   if (!sampleCode.value.length) {
     alert('Código de la muestra vacía')
@@ -24,13 +61,38 @@ const addItemToClinicalSamplesArray = () => {
     alert('El código no cumple el patrón requerido')
     return false;
   }
-  clinicalSamples.value.push(sampleCode.value)
-  sampleCode.value = ""
+
+  const payload = {
+    user_id: authUser.value.id,
+    codigo_muestra: sampleCode.value,
+    tipo_muestra: 'MUESTRA'
+  }
+
+  BatchService.saveBatchTemporal(payload)
+      .then((response) => {
+        if (response.data.statusCode !== 201) {
+          toast.error(response.data.message)
+        } else {
+          toast.success(response.data.message)
+          clinicalSamples.value.push(sampleCode.value)
+          sampleCode.value = ""
+        }
+      })
+      .catch((error) => {
+        toast.error(getError(error))
+      })
 }
 
 const removeItemToClinicalSamplesArray = (index) => {
   console.log(index)
 }
+
+const saveLote = async () => {
+  await console.log(clinicalSamples.value)
+}
+
+onMounted(getTemporalBatches)
+
 
 </script>
 
@@ -77,13 +139,21 @@ const removeItemToClinicalSamplesArray = (index) => {
         <div class="col-xl-8">
 
           <div class="row mb-3">
-            <div class="col">
+            <div class="col-10 d-flex align-items-center">
               <input v-model="filter"
-                     class="form-control"
+                     class="form-control me-5"
                      placeholder="Buscar código..."
                      type="text"
                      @keyup.prevent="filterValues"
               >
+            </div>
+            <div class="col-2 d-flex align-items-center">
+              <button class="btn btn-outline-success rounded"
+                      @click="saveLote"
+              >
+                <font-awesome-icon :icon="['fas', 'floppy-disk']"/>
+                Crear Lote
+              </button>
             </div>
           </div>
 
@@ -93,10 +163,10 @@ const removeItemToClinicalSamplesArray = (index) => {
             <div class="col">
               <button v-for="(item, index) in filteredItems"
                       :key="item"
-                      class="btn btn-outline-primary me-2"
+                      class="btn btn-outline-primary me-2 mb-2"
                       @click.prevent="removeItemToClinicalSamplesArray(index)"
               >
-                {{ item }}
+                MU{{ item.minv_formulario_id }}-1-{{ authUser.id }}
 
                 <font-awesome-icon :icon="['fas', 'times']" class="ms-2 text-danger"/>
 
---
+++ src/services/batchs/Batch.service.js
@@ -0,0 +1,13 @@
+import * as API from '@/services/API.js'
+
+export default {
+    getTemporalBatches(user_id, sede_id) {
+        return API.apiClient.get(`/api/v1/encuesta/get/tempmuestras/${user_id}/${sede_id}`)
+    },
+    saveBatchTemporal(payload) {
+        return API.apiClient.post(`/api/v1/encuesta/post/tempmuestras`, payload)
+    },
+    deleteBatchFromTemporal(payload) {
+        return API.apiClient.post(`/api/v1/encuesta/post/tempmuestras`, payload)
+    }
+}
