4b2d43e905b0682e4e33ca3eea7cb1048ec4cc8a
camilosalgadocastro <casc1902@hotmail.com>
2024-05-07 00:27:57 -0500

    camilo_limpiar state samples y countersamples 4

--- src/components/patients/subComponents/ShelfAssignment/SubComponents/ShelfClinicalCounterSamples.vue
+++ src/components/patients/subComponents/ShelfAssignment/SubComponents/ShelfClinicalCounterSamples.vue
@@ -2,12 +2,13 @@
 import {useStore} from "vuex";
 import {computed, ref} from "vue";
 import {useLoading} from "vue-loading-overlay";
-import shelfAssignmentService from "@/services/shelfAssignment/shelfAssignment.service.js";
 import {getError} from "@/utils/helpers/getError.js";
+import shelfAssignmentService from "@/services/shelfAssignment/shelfAssignment.service.js";
 
 const store = useStore()
 const authUser = computed(() => store.getters["auth/authUser"])
-const counterSampleCode = ref(null)
+const counterSampleCode = ref("")
+const shelfCode = ref("")
 const fullPage = ref(true)
 
 /**
@@ -34,60 +35,62 @@ const shelfCounterSampleAssignment = () => {
   const payload = {
     user_id: authUser.value.id,
     codigo_muestra: counterSampleCode.value,
-    codigo_ubicacion: '1-1-2-A'
+    codigo_ubicacion: shelfCode.value,// '1-1-2-A'
   }
   //CM5-FKHMMPLT-1-7
   //CM6-1RUJFAWI-1-7
   //samplesArray.value.push(payload)
 
-  if (!counterSampleCode.value.length) {
+  if (!counterSampleCode.value.length || !shelfCode.value.length) {
     Swal.fire({
       icon: 'error',
       title: 'Verifica!',
-      text: 'El campo de ContraMuestra No puede estar vacío'
+      text: 'El campo de ContraMuestra o de Ubicación No puede estar vacío'
     })
+    loader.hide()
+    return false;
+  }
+
+  if (regexCounterSamples.test(counterSampleCode.value)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Ooops!',
+      text: 'El Código de ContraMuestra NO cumple con el coincide con el patron'
+    })
+    loader.hide()
     return false;
   } else {
 
-    if (regexCounterSamples.test(counterSampleCode.value)) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Ooops!',
-        text: 'El Código de ContraMuestra NO cumple con el coincide con el patron'
-      })
-      return false;
-    }
-  }
-
-
-  shelfAssignmentService.saveShelfCounterSamplesAssignment(payload)
-      .then((response) => {
-        if (response.data.statusCode !== 201) {
+    shelfAssignmentService.saveShelfCounterSamplesAssignment(payload)
+        .then((response) => {
+          if (response.data.statusCode !== 201) {
+            loader.hide()
+            Swal.fire({
+              icon: 'error',
+              title: 'Oooops!',
+              text: response.data.message
+            })
+          } else {
+            loader.hide()
+            counterSampleCode.value = ""
+            shelfCode.value = ""
+            Swal.fire({
+              icon: 'success',
+              title: 'Perfectooo!',
+              text: response.data.message
+            })
+          }
+        })
+        .catch((error) => {
           loader.hide()
           Swal.fire({
             icon: 'error',
             title: 'Oooops!',
-            text: response.data.message
+            text: getError(error)
           })
-        } else {
-          loader.hide()
-          counterSampleCode.value = ""
-          Swal.fire({
-            icon: 'success',
-            title: 'Perfectooo!',
-            text: response.data.message
-          })
-        }
-      })
-      .catch((error) => {
-        loader.hide()
-        Swal.fire({
-          icon: 'error',
-          title: 'Oooops!',
-          text: getError(error)
         })
-      })
 
+  }
 }
 
 </script>
@@ -103,7 +106,7 @@ const shelfCounterSampleAssignment = () => {
 
     <div class="row">
       <div class="col">
-        <form autocomplete="off" @submit.prevent="shelfCounterSampleAssignment">
+        <form autocomplete="off">
 
           <div class="mb-3">
             <label class="form-label" for="inputCounterSamplesCode">Código de Contramuestra:</label>
@@ -117,11 +120,13 @@ const shelfCounterSampleAssignment = () => {
           </div>
 
           <div class="mb-5">
-            <label class="form-label" for="selectShelf">Seleccionar Ubicación de Asignación:</label>
-            <select id="selectShelf" class="form-select">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
+            <label class="form-label" for="inputShelfCode">Seleccionar Ubicación de Asignación:</label>
+            <input id="inputShelfCode"
+                   v-model="shelfCode"
+                   class="form-control"
+                   placeholder="Código de ubicación ContraMuestra"
+                   type="text"
+            >
           </div>
 
           <hr>
@@ -129,7 +134,8 @@ const shelfCounterSampleAssignment = () => {
           <div class="row">
             <div class="col text-end">
               <button class="btn btn-sm btn-outline-success"
-                      type="submit"
+                      type="button"
+                      @click.prevent="shelfCounterSampleAssignment"
               >
                 <font-awesome-icon :icon="['fas', 'warehouse']"/>
                 Asignar ContraMuestra a Estante
