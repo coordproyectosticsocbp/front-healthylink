ddd06963792ca5140936cbb7d08d9ce173c0d2cc
JAPI <jose.barrios@mibcode.com>
2024-05-19 23:55:09 -0500

    eCRF con valores

---
+++ src/utils/const/everyHowOften.js
@@ -0,0 +1,15 @@
+export const everyHowOften = [
+    {name: 'Cada 1 hora', value: 'Cada 1 hora'},
+    {name: 'Cada 2 hora', value: 'Cada 2 hora'},
+    {name: 'Cada 3 hora', value: 'Cada 3 hora'},
+    {name: 'Cada 4 hora', value: 'Cada 4 hora'},
+    {name: 'Cada 5 hora', value: 'Cada 5 hora'},
+    {name: 'Cada 6 hora', value: 'Cada 6 hora'},
+    {name: 'Cada 7 hora', value: 'Cada 7 hora'},
+    {name: 'Cada 8 hora', value: 'Cada 8 hora'},
+    {name: 'Cada 10 hora', value: 'Cada 10 hora'},
+    {name: 'Cada 12 hora', value: 'Cada 12 hora'},
+    {name: 'Cada 16 hora', value: 'Cada 16 hora'},
+    {name: 'Cada 24 hora', value: 'Cada 24 hora'},
+];
+//
