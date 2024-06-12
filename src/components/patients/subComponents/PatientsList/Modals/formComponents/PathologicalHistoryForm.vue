4d83ec9c5c4a5530b86cee2b69e65b2fb7f82a9c
camilosalgadocastro <casc1902@hotmail.com>
2024-06-11 23:26:31 -0500

    PATHOLOGICAL MODULE AND DOCKER COMPOSE 1.1.8 11062024 5

--- src/components/patients/subComponents/PatientsList/Modals/formComponents/PathologicalHistoryForm.vue
+++ src/components/patients/subComponents/PatientsList/Modals/formComponents/PathologicalHistoryForm.vue
@@ -1,19 +1,27 @@
 <script setup>
 
 import useLocalStorage from "@/composables/useLocalStorage.js";
+import {cie10Diseases} from "@/utils/const/cie10Diseases.js";
+import {ref} from "vue";
 
 const props = defineProps({
   itemIndexVal: Number
 })
 
-const pathologicalInfo = useLocalStorage(
-    {
-      patientPathologicalHistory: ''
-    }, `pathologicalInfo`
-)
+const pathology = ref('')
+const pathologicalInfo = useLocalStorage([], `pathologicalInfo`)
+
+const addItemToArray = () => {
+  pathologicalInfo.value.push(pathology.value)
+  pathology.value = ''
+}
+
+const removeDrugFromArray = (index) => {
+  pathologicalInfo.value.splice(index, 1)
+}
 
 function clearFields() {
-  pathologicalInfo.value.patientPathologicalHistory = ''
+  pathologicalInfo.value = []
 }
 
 defineExpose({
@@ -32,29 +40,68 @@ defineExpose({
     <div class="row mb-3">
       <div class="col text-center">
         <h6 class="fw-bold text-uppercase">
-          Hallazgos eCRF
+          Patologicos (CIE 10)
         </h6>
       </div>
     </div>
 
     <hr>
 
-    <div class="row">
+    <div class="row mb-4">
       <div class="col">
-        <form autocomplete="off">
-
-          <div class="mb-3">
-            <label class="form-label" for="pathologicalTextArea">Hallazgos:</label>
-            <textarea id="pathologicalTextArea" v-model="pathologicalInfo.patientPathologicalHistory"
-                      class="form-control"
-                      placeholder="Hallazgos eCRF"
-                      required
-                      rows="5"
-            />
+        <form autocomplete="off" @submit.prevent="addItemToArray">
+          <div class="row mb-5">
+            <div class="col">
+              <label class="form-label" for="pathologicalTextArea">Hallazgos:</label>
+              <VueMultiselect
+                  id="input-select-pathology"
+                  v-model="pathology"
+                  :close-on-select="true"
+                  :options="cie10Diseases"
+                  label="ci10"
+                  placeholder="Selecciones una patología"
+                  searchable
+                  track-by="ci10"
+              />
+            </div>
+            <!-- /.col -->
+            <div class="col d-flex align-items-center">
+              <button
+                  class="btn btn-sm btn-outline-secondary rounded-pill"
+                  type="submit"
+              >
+                <font-awesome-icon :icon="['fas', 'plus']"/>
+              </button>
+            </div>
+            <!-- /.col -->
           </div>
+          <!-- /.row -->
 
         </form>
       </div>
+    </div>
+
+    <div class="mb-3">
+      <h5 class="fw-bolder">Listado de patologías</h5>
+
+      <ol v-if="pathologicalInfo.length">
+        <li v-for="(item, index) in pathologicalInfo" :key="item.ci10">
+          <div class="d-flex justify-content-between align-items-center">
+
+            <p>{{ item.ci10 }}</p>
+
+            <div>
+              <button class="btn btn-sm rounded btn-outline-danger"
+                      @click="removeDrugFromArray(index)"
+              >
+                <font-awesome-icon :icon="['fas', 'trash']"/>
+              </button>
+            </div>
+
+          </div>
+        </li>
+      </ol>
+
     </div>
 
   </div>
